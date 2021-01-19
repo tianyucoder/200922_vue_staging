c37512d7d2fbc66b15b9cc48ca8f6cf367f0d70f
tianyu5177 <zhangtianyu5177@sina.com>
2021-01-19 11:21:03 +0800

    更新

--- 11_src_小复习/src/main.js
+++ 11_src_小复习/src/main.js
@@ -5,9 +5,9 @@ Vue.config.productionTip = false //关闭生产提示
 
 new Vue({
 	beforeCreate() {
-		Vue.prototype.$bus = this //安装事件总线
+		Vue.prototype.$bus = this
 	},
 	el:'#app',
 	render:h => h(App)
 })
-
+// console.log(vm)
